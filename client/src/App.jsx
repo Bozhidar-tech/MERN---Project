d9e2e136fcc3fd7bcebf2eaa5d3446a40e209630
Bozhidar Nunev <bozhidar.nunev@gmail.com>
2024-07-12 00:40:09 +0200

    Added Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
+import Profile from './pages/Profile/Profile';
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
