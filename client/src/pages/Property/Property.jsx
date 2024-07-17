945102e8ff9873e91608f86f4dab200ed26b4a8b
Bozhidar Nunev <bozhidar.nunev@gmail.com>
2024-07-17 04:18:05 +0200

    added property page

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -1,63 +1,143 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore from 'swiper';
-import { useSelector } from 'react-redux';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css/bundle';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { useSelector } from "react-redux";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
+import Contact from "../../components/Contact/Contact";
 
 export default function Property() {
-    SwiperCore.use([Navigation]);
-    const [property, setProperty] = useState(null);
-    const [loadingState, setLoadingState] = useState(false);
-    const [error, setError] = useState(false);
-    const params = useParams();
+  SwiperCore.use([Navigation]);
+  const [property, setProperty] = useState(null);
+  const [loadingState, setLoadingState] = useState(false);
+  const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
+  const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
 
-    useEffect(() => {
-
-        const fetchProperty = async () => {
-            try {
-                setLoadingState(true);
-                const res = await fetch(`/api/property/get/${params.propertyId}`);
-                const data = await res.json();
-                if(data.success === false){
-                    setError(true);
-                    setLoadingState(false);
-                    return;
-                }
-                setProperty(data);
-                setLoadingState(false);
-                setError(false);
-            } catch (error) {
-                setError(true);
-                setLoadingState(false);
-            }
-        };
-        fetchProperty();
-    }, [params.propertyId]);
+  useEffect(() => {
+    const fetchProperty = async () => {
+      try {
+        setLoadingState(true);
+        const res = await fetch(`/api/property/get/${params.propertyId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoadingState(false);
+          return;
+        }
+        setProperty(data);
+        setLoadingState(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoadingState(false);
+      }
+    };
+    fetchProperty();
+  }, [params.propertyId]);
 
   return (
-    <main className=''>
-      {loadingState && <p className='text-center my-7 text-2xl'>Loading...</p>}
-      {error && <p className='text-center my-7 text-2xl'>An error occurred.</p>}
+    <main>
+      {loadingState && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && <p className="text-center my-7 text-2xl">An error occurred.</p>}
       {property && !loadingState && !error && (
         <div>
-            <Swiper navigation>
-                {property.images.map((url) => (
+          <Swiper navigation>
+            {property.images.map((url) => (
               <SwiperSlide key={url}>
                 <div
-                  className='h-[550px]'
+                  className="h-[550px]"
                   style={{
                     background: `url(${url}) center no-repeat`,
-                    backgroundSize: 'cover',
+                    backgroundSize: "cover",
                   }}
                 ></div>
               </SwiperSlide>
             ))}
-            </Swiper>
+          </Swiper>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied!
+            </p>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {property.location}
+            </p>
+
+            <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {property.type === "house" ? "House" : "Apartment"}
+              </p>
+            </div>
+
+            <p className="text-slate-800">
+              <span className="font-semibold text-black">Description - </span>
+              {property.description}
+            </p>
+
+            <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaBed className="text-lg" />
+                {property.bedrooms > 1
+                  ? `${property.bedrooms} bedrooms `
+                  : `${property.bedrooms} bed `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaBath className="text-lg" />
+                {property.bathrooms > 1
+                  ? `${property.bathrooms} bathrooms `
+                  : `${property.bathrooms} bath `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaParking className="text-lg" />
+                {property.parking ? "Parking spot" : "No Parking"}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaChair className="text-lg" />
+                {property.furnished ? "Furnished" : "Unfurnished"}
+              </li>
+            </ul>
+
+            {currentUser &&
+              property.userRef !== currentUser._id &&
+              !contact && (
+                <button
+                  onClick={() => setContact(true)}
+                  className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+                >
+                  Contact landlord
+                </button>
+              )}
+            {contact && <Contact property={property} />}
+          </div>
         </div>
       )}
     </main>
-  )
+  );
 }
