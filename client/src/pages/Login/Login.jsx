a63a333c4c57bb0e906dc2f6d946bbf5ee07081b
Bozhidar Nunev <bozhidar.nunev@gmail.com>
2024-08-05 01:28:06 +0200

    added translation to login page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -3,10 +3,10 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginStart, loginSuccess, loginFailure } from '../../redux/user/userSlice.js';
 import OAuth from '../../components/OAuth/OAuth.jsx';
-
+import { useTranslation } from 'react-i18next';
 
 export default function Login() {
-
+    const { t } = useTranslation();
     const [formData, setFormData] = useState({});
     const { loading, error } = useSelector((state) => state.user);
     const navigate = useNavigate();
@@ -39,31 +39,33 @@ export default function Login() {
         dispatch(loginSuccess(data));
         navigate('/');
       } catch (error) {
-        dispatch(loginFailure(error.message));
+        dispatch(loginFailure(t('loginError')));
       }
     };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7 text-white'>
-        Вход
+        {t('login')}
       </h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="email" placeholder='Email' 
+        <input type="email" placeholder={t('emailPlaceholder')} 
         className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        <input type="password" placeholder='Парола' 
+        <input type="password" placeholder={t('passwordPlaceholder')} 
         className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
         <button disabled={loading} className='text-white p-3 rounded-lg uppercase hover:opacity-95 
-        disabled:opacity-80' style={{ backgroundColor: '#00B98E' }}>{loading ? 'Зареждане...' : 'Вход'}</button>
+        disabled:opacity-80' style={{ backgroundColor: '#00B98E' }}>
+          {loading ? t('loading') : t('loginButton')}
+        </button>
         <OAuth/>
       </form>
       <div className='flex gap-2 mt-5 text-white'>
-        <p>Нямате регистрация? </p>
+        <p>{t('registerPrompt')}</p>
         <Link to='/register'>
-        <span className='text-blue-500'>Регистрирайте се тук  </span>
+          <span className='text-blue-500'>{t('registerLink')}</span>
         </Link>
         <Link to='/forgot-password'>
-        <span className='text-red-500'>Забравена парола</span>
+          <span className='text-red-500'>{t('forgotPasswordLink')}</span>
         </Link>
       </div>
       
